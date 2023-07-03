316463092d33a20e83dd7631ca5a7c39b17338b8
Faraaz-Hussain <faraazhussain4722@gmail.com>
2023-07-03 19:47:44 +0530

    connection to nodejs server

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios';  
+
+function App() {
+  const [form,setForm]=useState({})
+  // const [data,setData]=useState()
+
+  
+  const handleForm = (e) => {
+  
+    setForm({
+      ...form,
+      [e.target.name] : e.target.value
+    })
+  }
+  
+const handleSubmit = async(e) => {
+  e.preventDefault()
+  const response=await axios.post('http://localhost:8080/demo',form)
+ console.log(response.data)
+}
+
+  return (
+    <div className="App">
+      <p>{JSON.stringify(form)}</p>
+      <form onSubmit={handleSubmit} >
+      <label htmlFor="email">Email:
+      <input type="text" id='email' name='Email' onChange={handleForm}/> 
+      </label>
+      <label htmlFor="password">password:
+      <input type="text" id='password' name='password' onChange={handleForm}/> 
+      <input type="submit" value="submit form" />
+      </label>
+      </form>
+    </div>
+  );
+}
+
+export default App;
